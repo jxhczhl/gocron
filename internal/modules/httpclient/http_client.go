2103601ae3c484ccc168d378bd9064438c6f1aa6
hliang <41224971+jxhczhl@users.noreply.github.com>
2022-02-26 16:06:09 +0800

    添加headers解析
    添加headers解析

--- internal/modules/httpclient/http_client.go
+++ internal/modules/httpclient/http_client.go
@@ -7,6 +7,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"strings"
 	"time"
 )
 
@@ -16,44 +17,46 @@ type ResponseWrapper struct {
 	Header     http.Header
 }
 
-func Get(url string, timeout int) ResponseWrapper {
+//多了个传参headers []string
+func Get(url string, timeout int, headers []string) ResponseWrapper {
 	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
 		return createRequestError(err)
 	}
-
-	return request(req, timeout)
+	return request(req, timeout, headers)
 }
 
-func PostParams(url string, params string, timeout int) ResponseWrapper {
+//多了个传参headers []string
+func PostParams(url string, params string, timeout int, headers []string) ResponseWrapper {
 	buf := bytes.NewBufferString(params)
 	req, err := http.NewRequest("POST", url, buf)
 	if err != nil {
 		return createRequestError(err)
 	}
 	req.Header.Set("Content-type", "application/x-www-form-urlencoded")
-
-	return request(req, timeout)
+	return request(req, timeout, headers)
 }
 
-func PostJson(url string, body string, timeout int) ResponseWrapper {
+//多了个传参headers []string
+func PostJson(url string, body string, timeout int, headers []string) ResponseWrapper {
 	buf := bytes.NewBufferString(body)
 	req, err := http.NewRequest("POST", url, buf)
 	if err != nil {
 		return createRequestError(err)
 	}
 	req.Header.Set("Content-type", "application/json")
-
-	return request(req, timeout)
+	return request(req, timeout, headers)
 }
 
-func request(req *http.Request, timeout int) ResponseWrapper {
+//多了个传参headers []string
+func request(req *http.Request, timeout int, headers []string) ResponseWrapper {
+	fmt.Println("进来了request")
 	wrapper := ResponseWrapper{StatusCode: 0, Body: "", Header: make(http.Header)}
 	client := &http.Client{}
 	if timeout > 0 {
 		client.Timeout = time.Duration(timeout) * time.Second
 	}
-	setRequestHeader(req)
+	setRequestHeader(req, headers)
 	resp, err := client.Do(req)
 	if err != nil {
 		wrapper.Body = fmt.Sprintf("执行HTTP请求错误-%s", err.Error())
@@ -68,12 +71,22 @@ func request(req *http.Request, timeout int) ResponseWrapper {
 	wrapper.StatusCode = resp.StatusCode
 	wrapper.Body = string(body)
 	wrapper.Header = resp.Header
-
 	return wrapper
 }
 
-func setRequestHeader(req *http.Request) {
-	req.Header.Set("User-Agent", "golang/gocron")
+func setRequestHeader(req *http.Request, headers []string) {
+	req.Header.Set("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36")
+	req.Header.Set("Cookie", "test")
+	//增加了
+	for _, value := range headers {
+		value = strings.Replace(value, "\"", "", -1)
+		value = strings.Replace(value, ",", "", -1)
+		headers_value := strings.Split(value, ":")
+		if len(headers_value) > 1 {
+			req.Header.Set(headers_value[0], headers_value[1])
+		}
+	}
+	//增加结束
 }
 
 func createRequestError(err error) ResponseWrapper {
